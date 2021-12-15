b909538ee5d11cd7aa00a55503729eb762b49cd5
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2021-12-15 17:54:52 +0200

    1

--- HW_04_Golovash_Anton_11.12.2021/User.cpp
+++ HW_04_Golovash_Anton_11.12.2021/User.cpp
@@ -1,1 +1,9 @@
 #include "User.h"
+
+User::User(int Id, string Name, string Surname, string PhoneNumber)
+{
+	_Id = Id;
+	_Name = Name;
+	_Surname = Surname;
+	_PhoneNumber = PhoneNumber;
+}
--- HW_04_Golovash_Anton_11.12.2021/User.h
+++ HW_04_Golovash_Anton_11.12.2021/User.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <ostream>
+using namespace std;
+
 class User
 {
+public:
+	User(int Id, string Name, string Surname, string PhoneNumber);
+
+private:
+	int _Id;
+	string _Name;
+	string _Surname;
+	string _PhoneNumber;
 };
 
